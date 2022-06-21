89e3088fc2030ac8b1ff9741e8f4517624c3fabc
devlipe <felipe.p.ferreira@ufv.br>
2022-06-20 21:45:55 -0300

    Add lualine with my config

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -154,6 +154,11 @@ _G.packer_plugins = {
     path = "/home/lipeferreira/.local/share/nvim/site/pack/packer/start/lspkind-nvim",
     url = "https://github.com/onsails/lspkind-nvim"
   },
+  ["lualine.nvim"] = {
+    loaded = true,
+    path = "/home/lipeferreira/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    url = "https://github.com/nvim-lualine/lualine.nvim"
+  },
   ["nlsp-settings.nvim"] = {
     loaded = true,
     path = "/home/lipeferreira/.local/share/nvim/site/pack/packer/start/nlsp-settings.nvim",
