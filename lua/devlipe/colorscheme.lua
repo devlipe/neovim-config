f9f02d8a89985e8aca51841f3800c57b6319b6c4
devlipe <felipe.p.ferreira@ufv.br>
2022-06-23 15:15:02 -0300

    New options for colorscheme

--- lua/devlipe/colorscheme.lua
+++ lua/devlipe/colorscheme.lua
@@ -1,4 +1,17 @@
+-- OneDark
+vim.g.NVIM_TUI_ENABLE_TRUE_COLOR = 1
+vim.g.onedark_hide_endofbuffer = 1
+vim.g.onedark_terminal_italics = 1
+
+-- TokyoNight
 vim.g.tokyonight_style = "night"
+vim.g.tokyonight_italic_keywords= 1
+vim.g.tokyonight_italic_comments= 1
+vim.g.tokyonight_lualine_bold= 1
+
+
+
+
 vim.cmd [[
 try
   colorscheme tokyonight
