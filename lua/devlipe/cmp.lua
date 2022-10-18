c464b1f1d73a05a629072db723d1a62db99e5f9d
Felipe Ferreira <felipe.p.ferreira@ufv.br>
2022-10-18 16:16:00 -0300

    Fix tabnine breaking

--- lua/devlipe/cmp.lua
+++ lua/devlipe/cmp.lua
@@ -80,7 +80,7 @@ local source_mapping = {
     -- nvim_lua = "[Lua]",
     path = "[Path]",
     luasnip = "[Snippet]",
-    cmp_tabnine = "[TN]",
+    --[[ cmp_tabnine = "[TN]", ]]
 
 }
 
@@ -144,12 +144,12 @@ cmp.setup {
         format = function(entry, vim_item)
             vim_item.kind = lspkind.presets.default[vim_item.kind]
             local menu = source_mapping[entry.source.name]
-            if entry.source.name == 'cmp_tabnine' then
-                if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-                    menu = entry.completion_item.data.detail .. ' ' .. menu
-                end
-                vim_item.kind = ''
-            end
+            --[[ if entry.source.name == 'cmp_tabnine' then ]]
+            --[[     if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then ]]
+            --[[         menu = entry.completion_item.data.detail .. ' ' .. menu ]]
+            --[[     end ]]
+            --[[     vim_item.kind = '' ]]
+            --[[ end ]]
             vim_item.menu = menu
             return vim_item
         end
